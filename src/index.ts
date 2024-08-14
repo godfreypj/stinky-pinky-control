b6a74b7437252542d463ea531ec46037a06af73f
philipjgodfrey <philipjgodfrey@gmail.com>
2024-08-14 11:15:39 +0000

    temporarily added workstation jwt until i get local figured out

--- src/index.ts
+++ src/index.ts
@@ -9,10 +9,10 @@ app.use(cors());
 dotenv.config();
 
 app.get('/', async (req, res) => {
-  const name = process.env.NAME || 'World';
-
   try {
-    const apiUrl = process.env.API_SERVICE; 
+    const apiUrl = process.env.API_SERVICE;
+    const workstationJwt = process.env.WORKSTATION_JWT;
+
     if (!apiUrl) {
       throw new Error('API_SERVICE environment variable not set');
     }
@@ -20,17 +20,15 @@ app.get('/', async (req, res) => {
     const response = await axios.get(apiUrl + 'api/generate', {
       baseURL: apiUrl,
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
+        'Cookie': workstationJwt
       },
-      withCredentials: true
+      withCredentials: true,
     });
 
     const apiData = response.data;
 
-    res.send(`Hello ${name}!
-
-    API Response:
-    ${JSON.stringify(apiData, null, 2)}`);
+    res.send(`${JSON.stringify(apiData, null, 2)}`);
   } catch (error) {
     console.error('Error fetching data from API:', error);
     res.status(500).send('Error fetching data from API' + error);
